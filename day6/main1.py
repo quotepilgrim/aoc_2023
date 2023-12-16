7933e1917406729403aca808f7e0efdb45b9c58d
QuotePilgrim <caio.psv@gmail.com>
2023-12-15 22:09:25 -0300

    day 6 part 1 start

--- day6/main1.py
+++ day6/main1.py
@@ -1,7 +1,14 @@
 import sys
 
 infile = sys.argv[1]
+data = {}
+name, values = ("", [])
 
 with open(infile, "r") as f:
     for line in f:
-        print(line.rstrip())
+        if line.strip():
+            name, values = line.split(":")
+            values = map(int, [i for i in values.split()])
+        data[name] = list(values)
+
+print(data)
