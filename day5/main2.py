76ef803781076137a9439a4bbe4659ced61a7aa6
QuotePilgrim <caio.psv@gmail.com>
2023-12-19 10:25:52 -0300

    day 5 part 2 done, with some help

--- day5/main2.py
+++ day5/main2.py
@@ -18,10 +18,46 @@ with open(infile, "r") as f:
         else:
             continue
 
-seed_ranges = [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]
+seed_ranges = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
 
-print(seed_ranges, maps)
 
-# It has become clear that if I'm going to solve this I'll have to restart
-# pretty much from scratch. It may take a very long time until I finally come
-# back to this with fresh eyes.
+def intersect(range_a, range_b):
+    off_bounds = []
+    intersection = []
+    highest_lower_bound = max(range_a[0], range_b[0])
+    lowest_upper_bound = min(range_a[1], range_b[1])
+
+    if highest_lower_bound < lowest_upper_bound:
+        intersection = (highest_lower_bound, lowest_upper_bound)
+        if highest_lower_bound > range_a[0]:
+            off_bounds.append((range_a[0], highest_lower_bound))
+        if lowest_upper_bound < range_a[1]:
+            off_bounds.append((lowest_upper_bound, range_a[1]))
+    return intersection, off_bounds
+
+
+results = []
+for key in maps:
+    map_ranges = maps[key]
+    results = []
+    while len(seed_ranges) > 0:
+        seed_start, seed_end = seed_ranges.pop()
+        for mapping in map_ranges:
+            dest_start, source_start = mapping[:2]
+            source_end = mapping[1] + mapping[2]
+
+            intersection, off_bounds = intersect(
+                (seed_start, seed_end), (source_start, source_end)
+            )
+
+            if intersection:
+                intersection = [dest_start + i - source_start for i in intersection]
+                results.append(tuple(intersection))
+                for i in off_bounds:
+                    seed_ranges.append(i)
+                break
+        else:
+            results.append((seed_start, seed_end))
+    seed_ranges = results
+
+print(min(results)[0])
