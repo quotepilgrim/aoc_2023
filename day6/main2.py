fa71ce0bacd37fa748a9fbe8a3143da2e3c0f713
QuotePilgrim <caio.psv@gmail.com>
2023-12-19 22:00:59 -0300

    day 6 part 2 done!

---
+++ day6/main2.py
@@ -0,0 +1,38 @@
+import sys
+import math
+
+infile = sys.argv[1]
+data = {}
+name, value = ("", "")
+
+with open(infile, "r") as f:
+    for line in f:
+        if line.strip():
+            name, value = line.split(":")
+            value = value.replace(" ", "").strip()
+        data[name] = int(value)
+
+
+def calculate(time, hold, record):
+    speed = hold
+    remaining = time - hold
+    distance = speed * remaining
+    return distance > record
+
+
+race_wins = []
+
+time = data["Time"]
+dist = data["Distance"]
+
+hold = 0
+wins = 0
+while hold < time:
+    hold += 1
+    if calculate(time, hold, dist):
+        wins += 1
+    elif wins > 0:
+        break
+race_wins.append(wins)
+
+print(math.prod(race_wins))
