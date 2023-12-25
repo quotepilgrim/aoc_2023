085257fa2da830b6a32e3df76197bb11fa7e47c0
QuotePilgrim <caio.psv@gmail.com>
2023-12-25 04:22:46 -0300

    consolidate all loops in d9

--- day9/main1.py
+++ day9/main1.py
@@ -18,24 +18,21 @@ with open(infile, "r") as f:
         inlists[i] = [[int(i) for i in line.strip().split()]]
         i += 1
 
-if reverse:
-    for k in inlists:
+result = 0
+for k in inlists:
+    if reverse:
         inlists[k][0].reverse()
 
-for k in inlists:
     while not all(i == 0 for i in inlists[k][-1]):
         old_list = inlists[k][-1]
-        new_list = [old_list[i+1] - old_list[i] for i in range(len(old_list)-1)]
+        new_list = [old_list[i + 1] - old_list[i] for i in range(len(old_list) - 1)]
         inlists[k].append(new_list)
 
-for k in inlists:
     for i in inlists[k]:
         i.append(0)
     for i in range(1, len(inlists[k]) + 1):
         inlists[k][-i][-1] = inlists[k][-i][-2] + inlists[k][-i + 1][-1]
 
-result = 0
-for k in inlists:
     result += inlists[k][0][-1]
 
 print(result)
